e81758a075ce1aff82c08c4914f04f02f277b224
pferronMend <112655467+pferronMend@users.noreply.github.com>
2023-05-23 15:14:49 -0500

    Update shorten_the_link.py

--- shorten_the_link.py
+++ shorten_the_link.py
@@ -1,1 +1,10 @@
 
+import pyshorteners
+
+link_address = input("enter the link: ")
+shortener_class_obj = pyshorteners.Shortener()
+
+# the shortened link
+result = shortener_class_obj.tinyurl.short(link_address)
+
+print(result)
